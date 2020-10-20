a4722df0dcb4f1dc2b9a603d53c5d8450b6415a0
Michael Lesser <mlesser@arizona.edu>
2020-10-20 11:58:27 -0700

    cleanup

--- azcam_mag/exposure_mag.py
+++ azcam_mag/exposure_mag.py
@@ -6,7 +6,8 @@ import os
 import time
 
 import azcam
-from azcam.exposures.exposure import Exposure, ReceiveData
+from azcam.exposures.exposure import Exposure
+from azcam.exposures.receive_data import ReceiveData
 
 
 class ExposureMag(Exposure):
@@ -124,9 +125,7 @@ class ExposureMag(Exposure):
 
         # transfer image data already read from controller
         try:
-            reply = self.receive_data.receive_image_data(
-                self.image.focalplane.numpix_image * 2
-            )
+            reply = self.receive_data.receive_image_data(self.image.focalplane.numpix_image * 2)
         except azcam.AzcamError:
             self.exposure_flag = azcam.db.exposureflags["ABORT"]
 
@@ -151,9 +150,7 @@ class ExposureMag(Exposure):
         self.exposure_flag = azcam.db.exposureflags["WRITING"]
 
         if self.image.remote_imageserver_flag:
-            local_file = (
-                self.temp_image_file + "." + self.filename.get_extension(self.filetype)
-            )
+            local_file = self.temp_image_file + "." + self.filename.get_extension(self.filetype)
             try:
                 os.remove(local_file)
             except FileNotFoundError:
@@ -172,12 +169,8 @@ class ExposureMag(Exposure):
         # update controller header with keywords which might have changed
         et = float(int(self.exposure_time_actual * 1000.0) / 1000.0)
         dt = float(int(self.dark_time * 1000.0) / 1000.0)
-        azcam.db.headers["exposure"].set_keyword(
-            "EXPTIME", et, "Exposure time (seconds)", float
-        )
-        azcam.db.headers["exposure"].set_keyword(
-            "DARKTIME", dt, "Dark time (seconds)", float
-        )
+        azcam.db.headers["exposure"].set_keyword("EXPTIME", et, "Exposure time (seconds)", float)
+        azcam.db.headers["exposure"].set_keyword("DARKTIME", dt, "Dark time (seconds)", float)
 
         # write file(s) to disk
         if self.save_file:
