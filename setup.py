6668cb55e1abe55e421d0139f66499ab7f990b9a
Michael Lesser <mlesser@arizona.edu>
2021-01-19 11:40:39 -0700

    cleanup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="azcam-mag",
-    version="21.1.2",
+    version="21.1.3",
     description="azcam extension for OCIW ITL Magellan CCD controllers",
     long_description=long_description,
     long_description_content_type="text/markdown",
